8cc8223fbfdbcb5af48a29ad8ba3e5df244c0598
giovannakaus <137062159+giovannakaus@users.noreply.github.com>
2024-11-01 11:45:13 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,38 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação.' 
+)
+
+criaCartao(
+    'Programação',
+    'O que é CSS?',
+    'O CSS é uma linguagem de estilização.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'O HTML é uma linguagem de marcação.' 
+)
+
+criaCartao(
+    'Programação',
+    'O que é Head?',
+    'O Head é usado como cabeçalho.'    
+)
+
+criaCartao(
+    'Programação',
+    'O que é Footer?',
+    ' O Footer é o conteudo final de uma página.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é Div?',
+    'O Div é um elemento HTML que serve como um container.'
+)
+
+
+
